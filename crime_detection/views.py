6cf813e2f3ff72915fa1e10b10b7f6ba5a078415
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-04-21 14:25:45 +0530

    uploading the updated files added the firebase database

--- crime_detection/views.py
+++ crime_detection/views.py
@@ -2,19 +2,26 @@ from django.shortcuts import render
 import pyrebase
 # Create your views here.
 config = {
-    apiKey: "AIzaSyAcv7rjtb0TPvaMoRdeqSlUUpiyjjNbioY",
-    authDomain: "realtime-crime-detection-773bc.firebaseapp.com",
-    projectId: "realtime-crime-detection-773bc",
-    storageBucket: "realtime-crime-detection-773bc.appspot.com",
-    messagingSenderId: "446675461946",
-    appId: "1:446675461946:web:c2b9e2b657e0932e3dd203"
+    "apiKey": "AIzaSyAcv7rjtb0TPvaMoRdeqSlUUpiyjjNbioY",
+    "authDomain": "realtime-crime-detection-773bc.firebaseapp.com",
+    "databaseURL": "https://realtime-crime-detection-773bc-default-rtdb.firebaseio.com",
+    "projectId": "realtime-crime-detection-773bc",
+    "storageBucket": "realtime-crime-detection-773bc.appspot.com",
+    "messagingSenderId": "446675461946",
+    "appId": "1:446675461946:web:c2b9e2b657e0932e3dd203"
 }
 
 firebase = pyrebase.initialize_app(config)
-auth = firebase.auth()
+authe = firebase.auth()
 database = firebase.database()
 
 
 def index(request):
-    data = database.child('')
-    return render(request, 'index.html')
+    Lattitude = database.child('Cordinates').child('Lattitude').get().val()
+    Longitude = database.child('Cordinates').child('Longitude').get().val()
+    Accuracy = database.child('Cordinates').child('Accuracy').get().val()
+    return render(request, 'index.html', {
+        "Lattitude": Lattitude,
+        "Longitude": Longitude,
+        "Accuracy": Accuracy
+    })
