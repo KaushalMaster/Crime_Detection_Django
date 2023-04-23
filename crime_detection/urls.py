35d2f45cac8ace0657dc1845a8717af58cab209b
Kaushal Soni <kaushal.sce20@sot.pdpu.ac.in>
2023-04-24 02:31:38 +0530

    uploading the crud functionality

--- crime_detection/urls.py
+++ crime_detection/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('result', views.result, name='result'),
-    path('register_complaint', views.register_complaint, name='register_complaint')
+    path('register_complaint', views.register_complaint, name='register_complaint'),
+    path('myadmin', views.myadmin, name='myadmin')
 ]
--- crime_detection/views.py
+++ crime_detection/views.py
@@ -8,6 +8,9 @@ from firebase_admin import db
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
+from google.oauth2 import service_account
+import google.auth.transport.requests
+import requests
 
 config = {
     "apiKey": "AIzaSyAcv7rjtb0TPvaMoRdeqSlUUpiyjjNbioY",
@@ -23,18 +26,25 @@ firebase = pyrebase.initialize_app(config)
 authe = firebase.auth()
 database = firebase.database()
 
+# Initialize Firebase app with credentials
+cred = credentials.Certificate(
+    'static\css\json\serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://realtime-crime-detection-773bc-default-rtdb.firebaseio.com'
+})
+
 
 def index(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        lattitude = data.get("lattitude")
-        longitude = data.get("longitude")
-        accuracy = data.get("accuracy")
-        print(lattitude)
-        print(longitude)
-        print(accuracy)
-        # Do something with the variable data
-        return HttpResponse(status=200)
+    # if request.method == "POST":
+    #     data = json.loads(request.body)
+    #     lattitude = data.get("lattitude")
+    #     longitude = data.get("longitude")
+    #     accuracy = data.get("accuracy")
+    #     print(lattitude)
+    #     print(longitude)
+    #     print(accuracy)
+    #     # Do something with the variable data
+    #     return HttpResponse(status=200)
 
     # if request.method == "POST":
     #     lattitude = request.POST.get('lattitude')
@@ -43,6 +53,12 @@ def index(request):
     #     print(lattitude)
     #     print(longitude)
     #     print(accuracy)
+
+    # if request.method == 'POST':
+    #     data = request.POST.get('data')
+    #     print(data.lattitude)
+    #     print(data.longitude)
+    #     print(data.accuracy)
     return render(request, 'index.html')
 
 
@@ -98,3 +114,72 @@ def register_complaint(request):
         return render(request, 'RegisterComplain.html')
 
     return render(request, 'RegisterComplain.html')
+
+
+def myadmin(request):
+    # complain_id = database.child('Crime_reports').child('id').get().val()
+    # complain_description = database.child('Crime_reports').child(
+    #     'complain_description').get().val()
+    # complain_date = database.child('Crime_reports').child('data').get().val()
+    # complain_by_name = database.child(
+    #     'Crime_reports').child('full_name').get().val()
+    # complain_by_phone = database.child(
+    #     'Crime_reports').child('phone').get().val()
+
+    # print(complain_id)
+    # print(complain_description)
+    # print(complain_date)
+    # print(complain_by_name)
+    # print(complain_by_phone)
+
+    # data = {
+    #     "complain_id": complain_id,
+    #     "complain_description": complain_description,
+    #     "complain_date": complain_date,
+    #     "complain_by_name": complain_by_name,
+    #     "complain_by_phone": complain_by_phone
+    # }
+
+    # cred = credentials.Certificate(
+    #     'static\css\json\serviceAccountKey.json')
+    # firebase_admin.initialize_app(cred, {
+    #     'databaseURL': 'https://realtime-crime-detection-773bc-default-rtdb.firebaseio.com'
+    # })
+
+    ref = db.reference()
+
+    # converting list to dictionary
+    data = []
+    snapshot = ref.child('Crime_reports').get()
+    for key, val in snapshot.items():
+        data.append(dict(val))
+
+    print(data)
+
+    complain_description = data[0]['complain_description']
+    complain_type = data[0]['complain_type']
+    complain_date = data[0]['date']
+    complain_by_name = data[0]['full_name']
+    complain_by_phone = data[0]['phone']
+
+    # last try
+    credentials = service_account.Credentials.from_service_account_file(
+        "static\css\json\serviceAccountKey.json"
+    )
+
+    access_token_info = credentials.fetch_oauth2_token()
+    access_token = access_token_info["access_token"]
+
+    response = requests.get(
+        "https://https://realtime-crime-detection-773bc-default-rtdb.firebaseio.com/static\css\json\serviceAccountKey.json?auth=" + access_token
+    )
+
+    print(response)
+    # complaints = data
+    return render(request, 'Admin.html', {
+        'descripxtion': complain_description,
+        'type': complain_type,
+        'date': complain_date,
+        'name': complain_by_name,
+        'phone': complain_by_phone
+    })
